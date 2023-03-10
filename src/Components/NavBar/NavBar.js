6534a1761bc20a61b2161fe69000afb78646d3ca
U.S.Khokhar <63156376+USKhokhar@users.noreply.github.com>
2023-03-10 13:17:06 +0530

    navbar-styled-large-screen

--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -1,82 +1,88 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { FiClock, FiPhoneCall, FiSearch } from 'react-icons/fi'
 import { GrLocation } from 'react-icons/gr'
+import { AiOutlineClose } from 'react-icons/ai'
 import { Link } from 'react-router-dom'
 
 const NavBar = () => {
+    const [ search, setSearch ] = useState(false)
   return (
     <header>
 
         {/* TOP PART OF HEADER WITH NAME AND INFORMATION */}
-        <article id="h-top">
+        <article id="h-top" className='w-full flex justify-around py-4 px-2'>
             <div id="h-top-name">
-                <h1>dr. ram saran garg</h1>
-                <h1>indo-german hospital</h1>
+                <h1 className='font-yeseva text-2xl text-primary uppercase'>dr. ram saran garg</h1>
+                <h1 className='font-yeseva text-2xl text-secondary uppercase'>indo-german hospital</h1>
             </div>
 
-            <div id="h-top-info">
-                <div className="h-top-info-container">
-                    <div className="icon">
-                        <FiPhoneCall />
+            <div id="h-top-info" className='flex flex-wrap gap-5'>
+                <div className="h-top-info-container flex items-center px-2">
+                    <div className="icon px-2">
+                        <FiPhoneCall size={30} />
                     </div>
                     <div className="text">
-                        <h3>Emergency</h3>
-                        <h4>(0120) 681-812-25</h4>
+                        <h3 className='font-work font-medium uppercase text-primary'>Emergency</h3>
+                        <h4 className='font-work text-secondary'>(0120) 681-812-25</h4>
                     </div>
                 </div>
-                <div className="h-top-info-container">
-                    <div className="icon">
-                        <FiClock />
+                <div className="h-top-info-container flex items-center px-2">
+                    <div className="icon px-2">
+                        <FiClock size={30} />
                     </div>
                     <div className="text">
-                        <h3>work hours</h3>
-                        <h4>09:00 - 20:00 everyday</h4>
+                        <h3 className='font-work font-medium uppercase text-primary'>work hours</h3>
+                        <h4 className='font-work text-secondary capitalize'>09:00 - 20:00 everyday</h4>
                     </div>
                 </div>
-                <div className="h-top-info-container">
-                    <div className="icon">
-                        <GrLocation />
+                <div className="h-top-info-container flex items-center px-2">
+                    <div className="icon px-2">
+                        <GrLocation size={30} />
                     </div>
                     <div className="text">
-                        <h3>location</h3>
-                        <h4>akgec, ghaziabad</h4>
+                        <h3 className='font-work font-medium uppercase text-primary'>location</h3>
+                        <h4 className='font-work text-secondary uppercase'>akgec, ghaziabad</h4>
                     </div>
                 </div>
             </div>
         </article>
 
         {/* BOTTOM PART OF HEADER WITH NAVIGATION AND SEARCH */}
-        <article id="h-bottom">
-            <nav>
+        <article id="h-bottom" className='bg-primary w-full flex justify-around items-center py-5'>
+            <nav className='flex justify-around gap-5'>
                 <Link to={'/'} >
-                    <p className="nav-link">Home</p>
+                    <p className="nav-link capitalize font-work text-white font-light hover:font-medium hover:text-accent">Home</p>
                 </Link>
                 <Link to={'/about'} >
-                    <p className="nav-link">about us</p>
+                    <p className="nav-link capitalize font-work text-white font-light hover:font-medium hover:text-accent">about us</p>
                 </Link>
                 <Link to={'/services'} >
-                    <p className="nav-link">services</p>
+                    <p className="nav-link capitalize font-work text-white font-light hover:font-medium hover:text-accent">services</p>
                 </Link>
                 <Link to={'/doctors'} >
-                    <p className="nav-link">doctors</p>
+                    <p className="nav-link capitalize font-work text-white font-light hover:font-medium hover:text-accent">doctors</p>
                 </Link>
                 <Link to={'/news'} >
-                    <p className="nav-link">news</p>
+                    <p className="nav-link capitalize font-work text-white font-light hover:font-medium hover:text-accent">news</p>
                 </Link>
                 <Link to={'/contact'} >
-                    <p className="nav-link">contact</p>
+                    <p className="nav-link capitalize font-work text-white font-light hover:font-medium hover:text-accent">contact</p>
                 </Link>
             </nav>
 
-            <div id='nav-side'>
-                <div className="search">
-                    <input type="search" />
-                    <button id='search-btn'>
-                        <FiSearch />
+            <div id='nav-side' className='flex gap-5 items-center'>
+                <div className="search flex items-center gap-2">
+                    {
+                        search ? <input type="search" className='rounded-full py-1.5 px-3 font-work text-sm outline-none' placeholder='Search...'/> : null
+                    }
+                    <button id='search-btn' onClick={() => setSearch(!search)}>
+                        {
+                            search ? <AiOutlineClose size={20} color={'white'} /> : <FiSearch size={25} color={'white'} />
+                        }
                     </button>
                 </div>
 
-                <button id="appoint-btn">
+                <button id="appoint-btn" className='bg-accent px-8 py-1.5 capitalize text-primary rounded-full font-semibold'>
                     appointment
                 </button>
             </div>
