d31b5410b621adf3e5d6db92351016a8bde550ca
U.S.Khokhar <63156376+USKhokhar@users.noreply.github.com>
2023-03-15 18:22:33 +0530

    navbar-fix

--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -7,6 +7,7 @@ import { Link } from 'react-router-dom'
 const NavBar = () => {
     const [ search, setSearch ] = useState(false)
     const [ isMenu, setIsMenu ] = useState(false)
+
   return (
     <header>
 
