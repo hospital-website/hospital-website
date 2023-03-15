95c7d8c0d5abd8327b83bc06d23f9357e044a043
U.S.Khokhar <63156376+USKhokhar@users.noreply.github.com>
2023-03-15 19:25:50 +0530

    hero-desktop

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -6,30 +6,42 @@ const Home = () => {
   return <>
     {/* HERO SECTION */}
 
-    <section id="hero">
+    <section id="hero" className='bg-hero-bg w-full h-[60vh] bg-no-repeat bg-cover relative'>
       
       {/* HERO TEXT CONTENT */}
 
-      <article id="hero-text">
-        <p id="pre-text">caring for life</p>
-        <h2 id="hero-text">Learning the Way in Media Excellence</h2>
+      <article id="hero-text" className='grid justify-start px-52 py-16'>
+        <p id="pre-text" className='font-work uppercase text-secondary font-semibold text-lg'>caring for life</p>
+        <h2 id="hero-text" className='font-yeseva text-5xl text-primary'>Learning the Way <br /> in Media Excellence</h2>
         
-        <button>our service</button>
+        <button className='bg-accent text-primary capitalize font-work text-base rounded-full py-2 px-6 font-bold justify-self-start'>our service</button>
       </article>
 
       {/*  HERO INTERACTIVE BUTTONS */}
 
-      <article id="hero-interactive">
-        <button>
-          Book an Appointment <AiOutlineCalendar />
+      <article id="hero-interactive" className='flex gap-3 justify-center my-[8vh]'>
+        <button className='w-fit flex items-center justify-center gap-3 py-6 px-3 bg-primary text-white font-work'>
+          <span>
+            Book an Appointment
+          </span> 
+          <span>
+            <AiOutlineCalendar size={35} />
+          </span>
         </button>
 
-        <button>
-          Online Consultation <IoIosPeople />
+        <button className='w-fit flex items-center justify-center gap-3 py-6 px-3 bg-accent text-primary font-work'>
+          <span>
+            Online Consultation
+          </span> 
+          <span>
+            <IoIosPeople size={35} />
+          </span>
+            
         </button>
         
-        <button>
-          Order Medicine Online <BsCashStack />
+        <button className='w-fit flex items-center justify-center gap-3 py-6 px-3 bg-secondary text-white font-work'>
+          <span>Order Medicine Online</span> 
+          <span><BsCashStack size={35} /></span>
         </button>
       </article>
     </section>
