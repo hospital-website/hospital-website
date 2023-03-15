ced59ba2bb17f028061893c136b7c58a619c013d
U.S.Khokhar <63156376+USKhokhar@users.noreply.github.com>
2023-03-15 18:24:50 +0530

    hero-section-structure

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,9 +1,40 @@
-import React from 'react'
+import { AiOutlineCalendar } from 'react-icons/ai'
+import { IoIosPeople } from 'react-icons/io'
+import { BsCashStack } from 'react-icons/bs'
 
 const Home = () => {
-  return (
-    <div>Home</div>
-  )
+  return <>
+    {/* HERO SECTION */}
+
+    <section id="hero">
+      
+      {/* HERO TEXT CONTENT */}
+
+      <article id="hero-text">
+        <p id="pre-text">caring for life</p>
+        <h2 id="hero-text">Learning the Way in Media Excellence</h2>
+        
+        <button>our service</button>
+      </article>
+
+      {/*  HERO INTERACTIVE BUTTONS */}
+
+      <article id="hero-interactive">
+        <button>
+          Book an Appointment <AiOutlineCalendar />
+        </button>
+
+        <button>
+          Online Consultation <IoIosPeople />
+        </button>
+        
+        <button>
+          Order Medicine Online <BsCashStack />
+        </button>
+      </article>
+    </section>
+
+  </>
 }
 
 export default Home
