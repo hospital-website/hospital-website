50d2375753e46004ab6f0e5931dac28cc4c2a664
Deepak Jain <deepakjain2k19@gmail.com>
2023-03-20 10:43:50 +0530

    About Us changes

--- src/Pages/About/About.js
+++ src/Pages/About/About.js
@@ -1,6 +1,7 @@
 import React from "react";
-
+import "./About.css";
 import Historybar from "../../Components/Historybar/Historybar";
+
 const About = () => {
   return (
     <>
@@ -60,6 +61,191 @@ const About = () => {
             Praesentium, alias vitae. alias vitae. vitae.
           </p>
         </div>
+      </section>
+
+      {/* ~~~~~~~~~~~~~Quote Section~~~~~~~~~~~~ */}
+      <section className="about-quotes  mt-[48px] md:mt-[72px] ">
+        <div
+          className=" h-[400px]  flex-col items-center  bg-about-quote bg-cover pt-[12px]"
+          style={{ boxShadow: "inset 0 0 0 1000px rgba(31,43,108,.65)" }}
+        >
+          <img
+            src="image_assets/aboutQuote.png"
+            className="max-h-[25px] max-w-[36px] mt-[48px] mb-[32px] mx-auto"
+            alt=""
+          />
+
+          <div className="qcorousel-body ">
+            <div id="qslider">
+              <input
+                type="radio"
+                name="qslider"
+                className="control-menu"
+                id="qs1"
+                defaultChecked
+              />
+
+              <input
+                type="radio"
+                name="qslider"
+                className="control-menu"
+                id="qs2"
+              />
+              <input
+                type="radio"
+                name="qslider"
+                className="control-menu"
+                id="qs3"
+              />
+              <label htmlFor="qs1" id="qslide1">
+                <div className="about-quote">
+                  <p className="text-[20px] text-center text-white max-w-3xl mx-auto">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Consectetur beatae culpa aliquam sit labore recusandae non
+                    cumque tempora alias inventore consequatur cum esse ut iure
+                    quis in, unde volupta1
+                  </p>
+                  <hr className="h-[1px] border-none bg-accent m-auto w-[20vw] my-[18px]" />
+                  <p className="text-[19px] text-accent text-center mb-[64px]">
+                    John Doe
+                  </p>
+                </div>
+              </label>
+              {/* <label htmlFor="qs2" id="qslide2">
+                <div className="about-quote">
+                  <p className="text-[20px] text-center text-white max-w-3xl mx-auto">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Consectetur beatae culpa aliquam sit labore recusandae non
+                    cumque tempora alias inventore consequatur cum esse ut iure
+                    quis in, unde volupta2
+                  </p>
+                  <hr className="h-[1px] border-none bg-accent m-auto w-[20vw] my-[18px]" />
+                  <p className="text-[19px] text-accent text-center mb-[64px]">
+                    John Doe
+                  </p>
+                </div>
+              </label>
+              <label htmlFor="qs3" id="qslide3">
+                <div className="about-quote">
+                  <p className="text-[20px] text-center text-white max-w-3xl mx-auto ">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Consectetur beatae culpa aliquam sit labore recusandae non
+                    cumque tempora alias inventore consequatur cum esse ut iure
+                    quis in, unde volupta3
+                  </p>
+                  <hr className="h-[1px] border-none m-auto bg-accent w-[20vw] my-[18px]" />
+                  <p className="text-[19px] text-accent text-center mb-[64px]">
+                    John Doe
+                  </p>
+                </div>
+              </label> */}
+            </div>
+          </div>
+          {/* <div className="control-buttons flex justify-center mb-8   z-343 ">
+            <div
+              id="cbtn1"
+              className=" w-[15px] h-[15px] rounded-full bg-white cursor-pointer mx-1 "
+            ></div>
+            <div
+              id="cbtn2"
+              className=" w-[15px] h-[15px] rounded-full bg-white cursor-pointer mx-1 "
+            ></div>
+            <div
+              id="cbtn3"
+              className=" w-[15px] h-[15px] rounded-full bg-white cursor-pointer mx-1 "
+            ></div>
+          </div> */}
+          <div className="control-buttons flex justify-center  mt-[270px]">
+            <div
+              id="cbtn1"
+              className=" w-[15px] h-[15px] rounded-full bg-white cursor-pointer mx-1 "
+            ></div>
+            <div
+              id="cbtn2"
+              className=" w-[15px] h-[15px] rounded-full bg-accent cursor-pointer mx-1 "
+            ></div>
+            <div
+              id="cbtn3"
+              className=" w-[15px] h-[15px] rounded-full bg-white cursor-pointer mx-1 "
+            ></div>
+          </div>
+        </div>
+
+        {/* <label htmlFor="qs1" id="qslide1">
+            <div
+              className="about-quote-1 flex flex-col items-center justify-center bg-about-quote bg-cover "
+              style={{ boxShadow: "inset 0 0 0 1000px rgba(31,43,108,.65)" }}
+            >
+              <img
+                src="image_assets/aboutQuote.png"
+                className="max-h-[25px] max-w-[36px] mt-[48px] mb-[32px]"
+                alt=""
+              />
+              <p className="text-[20px] text-center text-white max-w-3xl">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Consectetur beatae culpa aliquam sit labore recusandae non
+                cumque tempora alias inventore consequatur cum esse ut iure quis
+                in, unde volupta1
+              </p>
+              <hr className="h-[1px] border-none bg-accent w-[20vw] my-[18px]" />
+              <p className="text-[19px] text-accent mb-[64px]">John Doe</p>
+            </div>
+          </label>
+          <label htmlFor="qs2" id="qslide2">
+            <div
+              className="about-quote-1 flex flex-col items-center justify-center bg-about-quote bg-cover "
+              style={{ boxShadow: "inset 0 0 0 1000px rgba(31,43,108,.65)" }}
+            >
+              <img
+                src="image_assets/aboutQuote.png"
+                className="max-h-[25px] max-w-[36px] mt-[48px] mb-[32px]"
+                alt=""
+              />
+              <p className="text-[20px] text-center text-white max-w-3xl">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Consectetur beatae culpa aliquam sit labore recusandae non
+                cumque tempora alias inventore consequatur cum esse ut iure quis
+                in, unde volupta2
+              </p>
+              <hr className="h-[1px] border-none bg-accent w-[20vw] my-[18px]" />
+              <p className="text-[19px] text-accent mb-[64px]">John Doe</p>
+            </div>
+          </label>
+          <label htmlFor="qs3" id="qslide3">
+            <div
+              className="about-quote-1 flex flex-col items-center justify-center bg-about-quote bg-cover "
+              style={{ boxShadow: "inset 0 0 0 1000px rgba(31,43,108,.65)" }}
+            >
+              <img
+                src="image_assets/aboutQuote.png"
+                className="max-h-[25px] max-w-[36px] mt-[48px] mb-[32px]"
+                alt=""
+              />
+              <p className="text-[20px] text-center text-white max-w-3xl">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Consectetur beatae culpa aliquam sit labore recusandae non
+                cumque tempora alias inventore consequatur cum esse ut iure quis
+                in, unde volupta3
+              </p>
+              <hr className="h-[1px] border-none bg-accent w-[20vw] my-[18px]" />
+              <p className="text-[19px] text-accent mb-[64px]">John Doe</p>
+            </div>
+  </label>*/}
+
+        {/* <div className="control-buttons flex justify-center mb-8 absolute right-0 left-0 ml-auto mr-auto   z-343 mt-[270px]">
+          <div
+            id="cbtn1"
+            className=" w-[15px] h-[15px] rounded-full bg-white cursor-pointer mx-1 "
+          ></div>
+          <div
+            id="cbtn2"
+            className=" w-[15px] h-[15px] rounded-full bg-white cursor-pointer mx-1 "
+          ></div>
+          <div
+            id="cbtn3"
+            className=" w-[15px] h-[15px] rounded-full bg-white cursor-pointer mx-1 "
+          ></div>
+        </div> */}
       </section>
     </>
   );
