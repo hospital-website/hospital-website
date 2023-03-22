160ba71627c955af4a35168cf68fd6cfd2826159
Deepak Jain <deepakjain2k19@gmail.com>
2023-03-22 14:37:28 +0530

    Doctors Card Changes

--- src/Components/DoctorCard/DoctorCard.js
+++ src/Components/DoctorCard/DoctorCard.js
@@ -1,28 +1,38 @@
-import { useState } from 'react';
-import ReactCardFlip from 'react-card-flip';
+import { useState } from "react";
+import ReactCardFlip from "react-card-flip";
 
-const DoctorCard = ({image, name, role}) => {
-  const [isFlipped, setIsFlipped] = useState(false)
+const DoctorCard = ({ image, name, role }) => {
+  const [isFlipped, setIsFlipped] = useState(false);
 
   const handleFlip = () => {
-    setIsFlipped(!isFlipped)
-  }
-  return <div>
-    <ReactCardFlip isFlipped={isFlipped} flipDirection='vertical'>
-      <div className="aspect-[9/16] md:w-[15vw] w-[40vw] border border-gray-200 shadow-sm grid justify-between bg-accent" onMouseDown={handleFlip}>
-        <img src={image} alt="" />
-          <p className="px-2 font-work capitalize text-lg font-medium text-primary">{name}</p>
+    setIsFlipped(!isFlipped);
+  };
+  return (
+    <div>
+      <ReactCardFlip isFlipped={isFlipped} flipDirection="vertical">
+        <div
+          className="aspect-[9/16] md:w-[15vw] w-[40vw] border border-gray-200 shadow-sm grid justify-between bg-accent"
+          onMouseOver={handleFlip}
+          // onMouseLeave={handleFlip}
+        >
+          <img src={image} alt="" />
+          <p className="px-2 font-work capitalize text-lg font-medium text-primary">
+            {name}
+          </p>
           <p className="px-2 text-xs">{role}</p>
-    </div>
+        </div>
 
-        <article className='aspect-[9/16] md:w-[15vw] w-[40vw] bg-accent text-primary font-work text-lg font-medium' onMouseDown={handleFlip}>
+        <article
+          className="aspect-[9/16] md:w-[15vw] w-[40vw] bg-accent text-primary font-work text-lg font-medium"
+          onMouseLeave={handleFlip}
+        >
           <p>MD</p>
           <p>MBBS</p>
           <p>10 years of experience</p>
         </article>
-    </ReactCardFlip>
-
-  </div>
+      </ReactCardFlip>
+    </div>
+  );
 };
 
 export default DoctorCard;
--- src/Pages/Doctors/Doctors.js
+++ src/Pages/Doctors/Doctors.js
@@ -3,14 +3,27 @@ import Historybar from "../../Components/Historybar/Historybar";
 import DoctorCard from "./../../Components/DoctorCard/DoctorCard";
 
 const Doctors = () => {
-  
   return (
     <div>
       <Historybar page="doctors" pageData="Our Doctors" />
- 
-    <div className="card-container">
-      <DoctorCard image={'/image_assets/Doctor.png'} name='Dr. Anand Arora' role={'neurologist'} />
-    </div>
+
+      <div className="card-container flex my-10 items-center justify-around max-w-4xl m-auto">
+        <DoctorCard
+          image={"/image_assets/Doctor.png"}
+          name="Dr. Anand Arora"
+          role={"neurologist"}
+        />
+        <DoctorCard
+          image={"/image_assets/Doctor.png"}
+          name="Dr. Anand Arora"
+          role={"neurologist"}
+        />
+        <DoctorCard
+          image={"/image_assets/Doctor.png"}
+          name="Dr. Anand Arora"
+          role={"neurologist"}
+        />
+      </div>
     </div>
   );
 };
